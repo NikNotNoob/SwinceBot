d93e14d65c7555faff881bee465c41cb9c3c0136
Nicolas Lemay <nicolas.lemay10@gmail.com>
2021-08-04 12:42:40 -0400

    removed ask command

--- commands/ask.js
+++ commands/ask.js
@@ -4,7 +4,7 @@ const utilities = require("../utilities/webHookUtils");
 module.exports = {
 	name: "ask",
 	description: "Demande au prof, fait toi ghost.",
-	disabled: false,
+	disabled: true,
 	handle(message, args) {
 		utilities.getWebhook(message.channel).then((webhook) => {
 			let messageToSend = "***vu***";
@@ -14,8 +14,8 @@ module.exports = {
 					utilities.sendWebhookMessage(
 						webhook,
 						messageToSend,
-						"Domingo",
-						"https://pbs.twimg.com/profile_images/715220412217565184/xV-alGUk.jpg"
+						"Prof",
+						"https://4.bp.blogspot.com/-x1EtpB149Tk/Uia-0QSawiI/AAAAAAAABA8/RBDLmlJJZvY/s1600/facebook-default--profile-pic.jpg"
 					);
 				})
 				.catch((e) => {
